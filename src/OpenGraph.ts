e48fb262b3e911b7490078ec8d019f912c3aded9
Joe Jarvis <joe.jarvis109@gmail.com>
2020-11-15 20:38:22 -0500

    Open graph interface

--- src/OpenGraph.ts
+++ src/OpenGraph.ts
@@ -1,9 +1,20 @@
+export interface HybridGraph {
+    title?: string
+    description?: string
+    favicon?: string
+    url?: string
+}
+
+export interface OpenGraphResponse {
+    hybridGraph: HybridGraph
+}
+
 /**
  * Function to get site info from opengraph.io
  * @param apikey API key for opengraph.io
  * @param url URL to look up
  */
-export async function pageInfo(apikey: string, url: string): Promise<any> {
+export async function pageInfo(apikey: string, url: string): Promise<OpenGraphResponse> {
     let requestURL = new URL("https://opengraph.io/api/1.1/site/" + encodeURIComponent(url))
     requestURL.searchParams.append("accept_lang", "auto");
     requestURL.searchParams.append("app_id", apikey);
